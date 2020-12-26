a2a954e0a9da889e7eb3bdc9b95494585dad0d4b
ted(송성균) <ted.song@kakaocorp.com>
2020-12-26 21:10:34 +0900

    .

--- src/test/typescript/basic-types.test.ts
+++ src/test/typescript/basic-types.test.ts
@@ -1,3 +1,5 @@
+import Person from './main/Person'
+
 describe('Basic Types', () => {
     test('Enum#number', () => {
         enum Color {Red, Green, Blue}
@@ -7,7 +9,15 @@ describe('Basic Types', () => {
 
     test('Enum#name', () => {
         enum Color {Red = 1, Green, Blue}
+
         let colorName: string = Color[2]
         expect(colorName).toBe('Green')
+    })
+})
+
+describe('interface', () => {
+    test('test', () => {
+        let person : Person = {name: "Jane"}
+        expect(person.name).toBe("Jane");
     })
 })
